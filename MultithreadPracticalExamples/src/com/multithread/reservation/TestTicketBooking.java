59033969243aeeac681ca27484c0bdbea1767fcd
Rishikesh Mandal <me.rishi29@gmail.com>
2020-02-22 00:13:25 -0500

    Code formatted

--- MultithreadPracticalExamples/src/com/multithread/reservation/TestTicketBooking.java
+++ MultithreadPracticalExamples/src/com/multithread/reservation/TestTicketBooking.java
@@ -1,16 +1,16 @@
 package com.multithread.reservation;
 
 public class TestTicketBooking {
-	
- public static void main (String[]args) {
-	 
-	 TicketCounter ticketCounter = new TicketCounter();
-	 
-	 TicketBookingThread t1 = new TicketBookingThread(ticketCounter, "John",2);
-	 TicketBookingThread t2 = new TicketBookingThread(ticketCounter, "Bob",2);
-	 t1.start();
-	 t2.start();
-	 
- }
+
+	public static void main(String[] args) {
+
+		TicketCounter ticketCounter = new TicketCounter();
+
+		TicketBookingThread t1 = new TicketBookingThread(ticketCounter, "John", 2);
+		TicketBookingThread t2 = new TicketBookingThread(ticketCounter, "Bob", 2);
+		t1.start();
+		t2.start();
+
+	}
 
 }
